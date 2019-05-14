c13f8f9b21d24f8633cc8e951809f355dbc0c18f
Santiago Kiryluk Maclean <colorete@gmail.com>
2019-05-13 23:13:07 -0300

    firts Commit

---
+++ src/main/java/ImportExample.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import java.util.Random;
+
+public class ImportExample {
+	public static void main(String[] args) {
+		Random r = new Random();
+		System.out.println(r.nextInt(10)); // print a number between 0 and 9
+	}
+
+}
