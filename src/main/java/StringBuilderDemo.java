486c32c4146d60881f543819759970c372ab3ac5
Santiago Kiryluk Maclean <colorete@gmail.com>
2019-05-29 11:31:40 -0300

    Update StringBuilderDemo.java

---
+++ src/main/java/StringBuilderDemo.java
@@ -0,0 +1,13 @@
+package main.java;
+
+
+public class StringBuilderDemo {
+
+	public static void main(String[] args) {
+
+	    StringBuilder str = new StringBuilder();
+		System.out.println(str.length());
+
+	}
+
+}
